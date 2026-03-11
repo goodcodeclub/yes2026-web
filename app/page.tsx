555bcd77f7f040ec8446784af0fa09d1f1c3e879
Chris Kim <chris@goodcodeclub.com>
2026-03-11 10:08:04 -0400

    feat: implement redirect to home page on component mount

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import { ComponentExample } from "@/components/component-example";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
+
+  useEffect(() => {
+    
+    redirect("/home");
+    
+  }, []);
+
 return <>
-    <div className="flex items-center flex-col justify-center min-h-screen gap-2">
+    {/* <div className="flex items-center flex-col justify-center min-h-screen gap-2">
       <h1 className="m-0 ff-pack-hard text-white font-normal text-6xl">YES!26</h1>
       <p className="m-0 text-white">Coming soon</p>
-    </div>
+    </div> */}
 </>
 }
