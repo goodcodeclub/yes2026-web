3924a5e3c09b7e3c27f3fa565f5452172760bd5d
Chris Kim <chris@goodcodeclub.com>
2026-03-25 00:52:56 -0400

    feat: add initial environment sample file and new API routes for users

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function ProjectsPage() {
+    return (
+        <div className="min-h-screen bg-black text-white selection:bg-pink-500 selection:text-white">
+            LOGIN
+        </div>
+    )
+}
